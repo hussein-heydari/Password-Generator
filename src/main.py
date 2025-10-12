43e1f49906a9e303111ea744f7f25cfcdb688cf7
Hussein Heydari <albuspwbd99@gmail.com>
2025-10-12 23:42:31 +0330

    feat: Add basic elements for a password generator Add the base class for password genrators, to be expanded later, and a subclass for a password generator using random characters

---
+++ src/main.py
@@ -0,0 +1,42 @@
+import random
+import string
+
+class PasswordGenerator:
+    def __init__(self, pass_len):
+        self.pass_len = pass_len
+    
+    def generate():
+        pass
+
+class RandomPasswordGenerator(PasswordGenerator):
+    def __init__(self, pass_len, numbers=True, special_chars=True):
+        super().__init__(pass_len)
+        self.numbers = numbers
+        self.special_chars = special_chars
+
+    def get_necessary_part(self, pass_len):
+        necessary_lowercase = random.choice(string.ascii_lowercase)
+        necessary_uppercase = random.choice(string.ascii_uppercase)
+        necessary_pool = necessary_lowercase + necessary_uppercase
+        char_num = self.pass_len - 2
+        if self.numbers:
+            necessary_number = random.choice(string.digits)
+            necessary_pool += necessary_number
+            char_num -= 1
+        if self.special_chars:
+            necessary_special_char = random.choice(string.punctuation)
+            necessary_pool += necessary_special_char
+            char_num -= 1
+        return necessary_pool, char_num
+
+    def generate(self):
+        necessary_pool, char_num = self.get_necessary_part(self.pass_len)
+        print(necessary_pool, char_num)
+        main_pool = string.ascii_letters + string.digits + string.punctuation
+        rand_pool = ''.join(random.sample(main_pool, char_num))
+        print(rand_pool)
+        final_pool = list(necessary_pool + rand_pool)
+        print(final_pool)
+        random.shuffle(final_pool)
+        password = ''.join(final_pool)
+        return password
