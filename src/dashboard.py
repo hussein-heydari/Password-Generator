dbf082d3615b8f5748758915fd78467f1cd99b21
Hussein Heydari <albuspwbd99@gmail.com>
2025-11-01 00:51:20 +0330

    style: Improve the layout of the dashboard Set the page config with a title and an icon Add columns and containers for better visuals
    fix: Devise an error system for separator input
    Fix minor issues in password input handling

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,32 +1,49 @@
 import streamlit as st
+import string
 from main import RandomPasswordGenerator, MemorablePasswordGenerator, PinNumberGenerator
 
-st.title("WELCOME TO MY PASSWORD GENERATOR!", width="content")
+st.set_page_config(
+    page_title="Password Generator",
+    page_icon=":lock:",
+    layout="wide",
+)
 
-with st.container():
-    col1, col2, col3 = st.columns(3)
+'''
+# Password Generator Dashboard
+Generate secure and memorable passwords or PIN numbers using the options below.'''
 
-    with col1:
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    left_cell = col1.container(border=True,)
+    with left_cell:
         st.markdown("### Random Password")
-        pass_len = st.slider("Password length", 10, 20)
+        pass_len = st.slider("Password length", 8, 16, 12)
         st.write("Include in password:")
         prefrences = [st.checkbox('Numbers'), st.checkbox('Special Characters')]
         random_password = RandomPasswordGenerator(pass_len, prefrences[0], prefrences[1])
         if st.button("Generate Random Password"):
             st.write(random_password.generate())
 
-    with col2:
+with col2:
+    middle_cell = col2.container(border=True,)
+    with middle_cell:
         st.markdown("### Memorable Password")
-        word_count = st.slider("Number of words", 3, 8)
-        separator = st.text_input("Separator", "-")
+        word_count = st.slider("Number of words", 4, 8, 5)
+        separator = st.text_input("Separator", "-", max_chars=1)
         capitalize = st.checkbox("Capitalize Words")
         memorable_password = MemorablePasswordGenerator(word_count, separator, capitalize)
         if st.button("Generate Memorable Password"):
-            st.write(memorable_password.generate())
+            if not separator in string.punctuation:
+                st.error("You should use a special characther as seperator!")
+            else:
+                st.write(memorable_password.generate())
 
-    with col3:
+with col3:
+    right_cell = col3.container(border=True,)
+    with right_cell:
         st.markdown("### PIN Number")
-        pin_length = st.slider("PIN length", 3, 8)
+        pin_length = st.slider("PIN length", 7, 14, 10)
         pin_number = PinNumberGenerator(pin_length)
         if st.button("Generate PIN Number"):
             st.write(pin_number.generate())
